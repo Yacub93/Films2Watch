3e5bda6745d4721d6ef8c168617f97f71dc73e0d
Yacub Ali <Yacub93@Yacubs-MacBook-Pro.local>
2017-07-16 21:47:13 +0100

    filter input added to autocomplete

--- js/controllers/searchFilmController.js
+++ js/controllers/searchFilmController.js
@@ -1,33 +1,48 @@
 'use strict';
 
-filmApp.controller('SearchController',function ($scope, $http){
-	// var vm = this;
+filmApp.controller('SearchController',function ($scope, $http, $log){
+	
     $scope.results = {
       values: []
     };
+    var vm = this;
+    vm.querySearch   = querySearch;
+    vm.searchTextChange = searchTextChange;
+    vm.selectedItemChange = selectedItemChange;
 
-    // vm.data = null;
-    // vm.searchText = null;
-    // vm.selectedItem = null;
-    // vm.throttle = 300;
-	// var filmTitle = $scope.formData.searchTerm;
 
-//querySearch method returns a promise & from the promise, it returns film data.
-$scope.querySearch = function (query) {
-  return $http.get('https://api.themoviedb.org/3/search/movie?include_adult=false&page=1', {
+//querySearch() returns a promise & from the promise, it returns film Title data.
+ function querySearch(query) {
+   return $http.get('https://api.themoviedb.org/3/search/movie?include_adult=false&page=1&primary_release_year=2017', {
          params: { 
              'query': query,
              'api_key': apiKey
          }
-  }).then(function (response, status) {
-       var data = response.data;
-       for (var i = 0; i < data.results.length; i++) {
+  }).then(function(response) {
+       var data = response.data.results.filter(function(obj) {
+        // $log.info(obj);
+        return obj.original_title.toLowerCase().indexOf(query) != -1;
+    })
+       // $log.info(data);
+    return data;
+
+        for (var i = 0; i < data.results.length; i++) {
            $scope.results.values.push({title: data.results[i].original_title});
-           // console.log($scope.results.values);      
+           // $log.info($scope.results.values);   
        }
        return $scope.results.values;
     })
 };
+ 
+
+function searchTextChange(text) {
+    $log.info('Search Text changed to ' + text);
+}
+
+function selectedItemChange(item) {
+  //TODO: return film data based on selection
+    $log.info('Item changed to ' + JSON.stringify(item));
+}
 
 
 });
