21019cbda1fbe46e98653a38f0ac436b5d35ccae
Yacub Ali <Yacub93@Yacubs-MacBook-Pro.local>
2017-08-11 20:57:56 +0100

    Minor changes

--- connection.js
+++ connection.js
@@ -1,2 +1,13 @@
+var env = process.env.NODE_ENV || 'dev';
+var con = '';
+
+if (env==='dev') {
+	con = 'mongodb://localhost:27017/filmapp';
+} else {
+   con = 'mongodb://Yacub93:Yacub1993Ali@ds129143.mlab.com:29143/film-app';
+}
+
+console.log(con);
+exports.connectionString = con;
 // exports.connectionString = 'mongodb://localhost:27017/filmapp';
-exports.connectionString = 'mongodb://Yacub93:Yacub1993Ali@ds129143.mlab.com:29143/film-app';
+// exports.connectionString = 'mongodb://Yacub93:Yacub1993Ali@ds129143.mlab.com:29143/film-app';
