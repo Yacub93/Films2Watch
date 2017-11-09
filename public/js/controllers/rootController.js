2188d2a1cc5573a1cf818cb1e223288b15098dda
Yacub Ali <Yacub93@Yacubs-MacBook-Pro.local>
2017-11-09 18:02:11 +0000

    Enhancements

--- public/js/controllers/rootController.js
+++ public/js/controllers/rootController.js
@@ -8,6 +8,10 @@ filmApp.controller('rootController',['$scope', function($scope){
 	      githubprofile: "https://github.com/Yacub93",
 	      linkedinprofile: "https://uk.linkedin.com/in/yacub-ali-4898b9103"
 	    }
+	}
+
+	$scope.footer = {
+		copyright: "Yacub A. Ali 2017. All Rights Reserved"
 	};
 
 }]);
